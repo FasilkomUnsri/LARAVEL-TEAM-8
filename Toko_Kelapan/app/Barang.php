2dd47e6b97e07a61cdd99073aa65dca3ef8a5c88
aditiapratiwi <aditiaprtw@gmail.com>
2021-11-27 20:21:32 +0700

    Push-Bagian-AditiaPratiwi

--- Toko_Kelapan/app/Barang.php
+++ Toko_Kelapan/app/Barang.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Barang extends Model
 {
-    //
+    public function Pesanan_Detail(){
+        return $this->hasMany('App\Models\Pesanan_Detail','barang_id','id');
+    }
 }
