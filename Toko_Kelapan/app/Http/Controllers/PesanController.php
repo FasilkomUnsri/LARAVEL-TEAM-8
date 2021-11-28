d7e137194dd1f633c74dcf9370aa8bd971d8be36
Andii <90497419+AandiSuryawan@users.noreply.github.com>
2021-11-28 20:39:56 +0700

    Perbaikan

--- Toko_Kelapan/app/Http/Controllers/PesanController.php
+++ Toko_Kelapan/app/Http/Controllers/PesanController.php
@@ -17,68 +17,68 @@ class PesanController extends Controller
         $this->middleware('auth');
     }
     public function index($id){
-        $barang = Barang::where('id', $id)->first();
-        return view('pesan.index', compact('barang'));
+    	$barang = Barang::where('id', $id)->first();
+    	return view('pesan.index', compact('barang'));
     }
-    public function pesan(Request $request, $id)
-    {   
-        $barang = Barang::where('id', $id)->first();
-        $tanggal = Carbon::now();
+	public function pesan(Request $request, $id)
+    {	
+    	$barang = Barang::where('id', $id)->first();
+    	$tanggal = Carbon::now();
 
-        //validasi apakah melebihi stok
-        if($request->jumlah_pesan > $barang->stok)
-        {
-            return redirect('pesan/'.$id);
-        }
+    	//validasi apakah melebihi stok
+    	if($request->jumlah_pesan > $barang->stok)
+    	{
+    		return redirect('pesan/'.$id);
+    	}
 
-        //cek validasi
-        $cek_pesanan = Pesanan::where('user_id', Auth::user()->id)->where('status',0)->first();
-        //simpan ke database pesanan
+    	//cek validasi
+    	$cek_pesanan = Pesanan::where('user_id', Auth::user()->id)->where('status',0)->first();
+    	//simpan ke database pesanan
         if(empty($cek_pesanan))
         {
-            $pesanan = new Pesanan;
-            $pesanan->user_id = Auth::user()->id;
-            $pesanan->tanggal = $tanggal;
-            $pesanan->status = 0;
-            $pesanan->jumlah_harga = 0;
-            $pesanan->save();
-        }
-        
+    		$pesanan = new Pesanan;
+	    	$pesanan->user_id = Auth::user()->id;
+	    	$pesanan->tanggal = $tanggal;
+	    	$pesanan->status = 0;
+	    	$pesanan->jumlah_harga = 0;
+	    	$pesanan->save();
+    	}
+    	
 
-        //simpan ke database pesanan detail
-        $pesanan_baru = Pesanan::where('user_id', Auth::user()->id)->where('status',0)->first();
+    	//simpan ke database pesanan detail
+    	$pesanan_baru = Pesanan::where('user_id', Auth::user()->id)->where('status',0)->first();
 
-        //cek pesanan detail
-        $cek_pesanan_detail = PesananDetail::where('barang_id', $barang->id)->where('pesanan_id', $pesanan_baru->id)->first();
-        if(empty($cek_pesanan_detail))
+    	//cek pesanan detail
+    	$cek_pesanan_detail = PesananDetail::where('barang_id', $barang->id)->where('pesanan_id', $pesanan_baru->id)->first();
+    	if(empty($cek_pesanan_detail))
         {
-            $pesanan_detail = new PesananDetail;
-            $pesanan_detail->barang_id = $barang->id;
-            $pesanan_detail->pesanan_id = $pesanan_baru->id;
-            $pesanan_detail->jumlah = $request->jumlah_pesan;
-            $pesanan_detail->jumlah_harga = $barang->harga*$request->jumlah_pesan;
-            $pesanan_detail->save();
-        }else {
-            $pesanan_detail = PesananDetail::where('barang_id', $barang->id)->where('pesanan_id', $pesanan_baru->id)->first();
+    		$pesanan_detail = new PesananDetail;
+	    	$pesanan_detail->barang_id = $barang->id;
+	    	$pesanan_detail->pesanan_id = $pesanan_baru->id;
+	    	$pesanan_detail->jumlah = $request->jumlah_pesan;
+	    	$pesanan_detail->jumlah_harga = $barang->harga*$request->jumlah_pesan;
+	    	$pesanan_detail->save();
+    	}else {
+    		$pesanan_detail = PesananDetail::where('barang_id', $barang->id)->where('pesanan_id', $pesanan_baru->id)->first();
 
-            $pesanan_detail->jumlah = $pesanan_detail->jumlah+$request->jumlah_pesan;
+    		$pesanan_detail->jumlah = $pesanan_detail->jumlah+$request->jumlah_pesan;
             
-            //harga sekarang
-            $harga_pesanan_detail_baru = $barang->harga*$request->jumlah_pesan;
-            $pesanan_detail->jumlah_harga = $pesanan_detail->jumlah_harga+$harga_pesanan_detail_baru;
-            $pesanan_detail->update();
-        }
-        //jumlah total
-        $pesanan = Pesanan::where('user_id', Auth::user()->id)->where('status',0)->first();
-        $pesanan->jumlah_harga = $pesanan->jumlah_harga+$barang->harga*$request->jumlah_pesan;
-        $pesanan->update();
+    		//harga sekarang
+    		$harga_pesanan_detail_baru = $barang->harga*$request->jumlah_pesan;
+	    	$pesanan_detail->jumlah_harga = $pesanan_detail->jumlah_harga+$harga_pesanan_detail_baru;
+	    	$pesanan_detail->update();
+    	}
+		//jumlah total
+    	$pesanan = Pesanan::where('user_id', Auth::user()->id)->where('status',0)->first();
+    	$pesanan->jumlah_harga = $pesanan->jumlah_harga+$barang->harga*$request->jumlah_pesan;
+    	$pesanan->update();
 
         return redirect('check-out');
-    }
-    public function check_out()
+	}
+	public function check_out()
     {
         $pesanan = Pesanan::where('user_id', Auth::user()->id)->where('status',0)->first();
-    $pesanan_details = [];
+ 	$pesanan_details = [];
         if(!empty($pesanan))
         {
             $pesanan_details = PesananDetail::where('pesanan_id', $pesanan->id)->get();
@@ -87,30 +87,29 @@ class PesanController extends Controller
         
         return view('pesan.check_out', compact('pesanan', 'pesanan_details'));
     }
-    public function delete($id){
-        $pesanan_detail = PesananDetail::where('id', $id)->first();
+	public function delete($id){
+		$pesanan_detail = PesananDetail::where('id', $id)->first();
 
-        $pesanan = Pesanan::where('id', $pesanan_detail->pesanan_id)->first();
-        $pesanan->jumlah_harga = $pesanan->jumlah_harga-$pesanan_detail->jumlah_harga;
-        $pesanan->update();
-        
-        $pesanan_detail->delete();
-        return redirect('check-out');
-    }
-    public function konfirmasi(){
-        $pesanan = Pesanan::where('user_id', Auth::user()->id)->where('status',0)->first();
-        $pesanan_id = $pesanan->id;
-        $pesanan->status = 1;
-        $pesanan->update();
-        
-        $pesanan_details = PesananDetail::where('pesanan_id', $pesanan_id)->get();
-        foreach($pesanan_details as $pesanan_detail){
-            $barang = Barang::where('id', $pesanan_detail->barang_id)->first();
-            $barang->stok = $barang->stok-$pesanan_detail->jumlah;
-            $barang->update();
-        }
+		$pesanan = Pesanan::where('id', $pesanan_detail->pesanan_id)->first();
+		$pesanan->jumlah_harga = $pesanan->jumlah_harga-$pesanan_detail->jumlah_harga;
+		$pesanan->update();
+		
+		$pesanan_detail->delete();
+		return redirect('check-out');
+	}
+	public function konfirmasi(){
+		$pesanan = Pesanan::where('user_id', Auth::user()->id)->where('status',0)->first();
+		$pesanan_id = $pesanan->id;
+		$pesanan->status = 1;
+		$pesanan->update();
+		
+		$pesanan_details = PesananDetail::where('pesanan_id', $pesanan_id)->get();
+		foreach($pesanan_details as $pesanan_detail){
+			$barang = Barang::where('id', $pesanan_detail->barang_id)->first();
+			$barang->stok = $barang->stok-$pesanan_detail->jumlah;
+			$barang->update();
+		}
 
-        return redirect('check-out');
-    }
+		return redirect('check-out');
+	}
 }
-
--- Toko_Kelapan/resources/views/home.blade.php
+++ Toko_Kelapan/resources/views/home.blade.php
@@ -3,21 +3,24 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
+        @foreach($barangs as $barang)
+        <div class="col-md-4">
+            <div class="card" >
+                <img src="{{ url('uploads') }}/{{ $barang->gambar }}" class="card-img-top" alt="...">
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <h5 class="card-title">{{ $barang->nama_barang }}</h5>
+                    <p class="card-text">
+                        <strong>Harga :</strong> Rp. {{ number_format($barang->harga)}} <br>
+                        <strong>Stok :</strong> {{ $barang->stok }} <br>
+                        <hr>
+                        <strong>Keterangan :</strong> <br>
+                        {{ $barang->keterangan }} 
+                    </p>
+                    <a href="{{ url('pesan') }}/{{ $barang->id }}" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Pesan</a>
                 </div>
             </div>
         </div>
+        @endforeach
     </div>
 </div>
 @endsection
