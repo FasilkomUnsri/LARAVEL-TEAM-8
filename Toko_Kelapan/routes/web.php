76be00c59c47db12f3cd88cb748f023ea89522b0
amelia safitri <amelias963@gmail.com>
2021-11-28 12:20:05 +0700

    web-amelia

--- Toko_Kelapan/routes/web.php
+++ Toko_Kelapan/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,3 +20,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('pesan/{id}', 'PesanController@index');
+Route::post('pesan/{id}', 'PesanController@pesan');
+Route::get('check-out', 'PesanController@check_out');
+Route::delete('check-out/{id}', 'PesanController@delete');
+
+Route::get('konfirmasi-check-out', 'PesanController@konfirmasi');
